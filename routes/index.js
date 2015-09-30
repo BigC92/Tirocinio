8867a238e60368e5ccb7c4cbd5a293dc82dc7e9a
Cristiano Tofani <cristianotofani@MBPdiCristiano.lan>
2015-09-30 17:26:21 +0200

    Form di richiesta centro

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var base64Img = require('base64-img');
+var request = require("request");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var url = 'https://maps.googleapis.com/maps/api/staticmap?zoom=2&size=640x640&scale=2&key=AIzaSyBmYC8wXlEiF1ZNIhjWJCVkVilzWA_VfeA';
-  base64Img.requestBase64(url, function(e, r, body) {
-    res.render('map', {url: body});
-  });
+router.get('/image/:center', function(req, res) {
+  var url = 'https://maps.googleapis.com/maps/api/staticmap?zoom=5&size=640x640&key=AIzaSyBmYC8wXlEiF1ZNIhjWJCVkVilzWA_VfeA&center='
+  request(url+req.params.center).pipe(res);
 });
+
+router.post("/map", function (req, res){
+  res.render("map", req.body);
+})
 
 module.exports = router;
